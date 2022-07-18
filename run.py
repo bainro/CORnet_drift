8d6efb234ec58db444216897404bb2afbc56c3ae
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-07-18 10:09:16 -0700

    Update run.py

--- run.py
+++ run.py
@@ -269,6 +269,8 @@ class CIFAR100Train(object):
         if FLAGS.ngpus > 0:
             target = target.cuda(non_blocking=True)
         output = self.model(inp)
+        print(output.shape)
+        assert False, "did it on porpoise"
 
         record = {}
         loss = self.loss(output, target)
