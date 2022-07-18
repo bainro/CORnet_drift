c9cb07f597249843eccfd7df1617078f20ad7312
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-07-18 15:42:25 -0700

    Update __init__.py

--- cornet/__init__.py
+++ cornet/__init__.py
@@ -20,7 +20,7 @@ def get_model(model_letter, pretrained=False, map_location=None, **kwargs):
         url = f'https://s3.amazonaws.com/cornet-models/cornet_{model_letter.lower()}-{model_hash}.pth'
         ckpt_data = torch.utils.model_zoo.load_url(url, map_location=map_location)
         # removing the last layer's weights since they're for ImageNet classes, not CIFAR100
-        mod_weights = removekey(ckpt_data['state_dict'],['module.decoder.linear.weight',])
+        mod_weights = removekey(ckpt_data['model'],['module.decoder.linear.weight',])
         model.load_state_dict(mod_weights['state_dict'], strict=False)
     return model
 
