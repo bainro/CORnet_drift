6c4a18fdf2674af755ef808e53ce6ff35cb07142
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-07-21 15:44:05 -0700

    more typos found thru running/debugging

--- run.py
+++ run.py
@@ -315,7 +315,7 @@ class CIFAR100Val(object):
             download=True, transform=transform,
         )
 
-        num_test = len(train_dataset)
+        num_test = len(test_dataset)
         indices = list(range(num_test))
         split = int(np.floor(movie_size * num_test))
 
